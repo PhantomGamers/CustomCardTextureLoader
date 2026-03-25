8925e142a935d6132d4dfe14414842ceae28c86d
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2026-03-25 17:41:31 -0400

    Update MainFile.cs

--- MainFile.cs
+++ MainFile.cs
@@ -1,6 +1,5 @@
 using CustomCardTextureLoader.CustomCardTextureLoaderCode;
 using Godot;
-using GodotPlugins.Game;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Modding;
 
@@ -12,7 +11,7 @@ public partial class MainFile : Node
     private const string
         ModId = "CustomCardTextureLoader";
 
-    public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
+    private static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
         new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
     
     public static void Initialize()
